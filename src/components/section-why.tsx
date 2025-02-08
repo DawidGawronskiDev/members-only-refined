0fed270661562bb97bebd1e01fa6dd60adf55e3a
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-02-08 13:29:55 +0000

    update why section styling

--- src/components/section-why.tsx
+++ src/components/section-why.tsx
@@ -7,39 +7,29 @@ export default function SectionWhy() {
       <MaxWidth>
         <div className="flex flex-col items-center gap-16">
           <h2>Why Join?</h2>
-          <div className="flex flex-col md:flex-row items-center md:items-start gap-4">
-            <div className="flex flex-col items-center p-6 text-center w-full">
-              <Image
-                src="/sparkle.svg"
-                alt=""
-                width={20}
-                height={20}
-                className="mb-2"
-              />
-              <h3>Exclusive Access</h3>
-              <p>Not everyone gets in.</p>
+          <div className="w-full grid md:grid-cols-3 md:flex-row items-center md:items-start gap-4">
+            <div className="flex flex-col items-center gap-4 w-full h-full border border-gray-100/10 p-4 rounded-2xl bg-gray-100/5 shadow-sm">
+              <Image src="/sparkle.svg" alt="" width={20} height={20} />
+              <div className="text-center">
+                <h3>Exclusive Access</h3>
+                <p className="font-extralight">Not everyone gets in.</p>
+              </div>
             </div>
-            <div className="flex flex-col items-center p-6 text-center w-full">
-              <Image
-                src="/shield.svg"
-                alt=""
-                width={20}
-                height={20}
-                className="mb-2"
-              />
-              <h3>Privacy First</h3>
-              <p>No public visibility, no distractions.</p>
+            <div className="flex flex-col items-center gap-4 w-full h-full border border-gray-100/10 p-4 rounded-2xl bg-gray-100/5 shadow-sm">
+              <Image src="/shield.svg" alt="" width={20} height={20} />
+              <div className="text-center">
+                <h3>Privacy First</h3>
+                <p className="font-extralight">
+                  No public visibility, no distractions.
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col items-center p-6 text-center w-full">
-              <Image
-                src="/users-round.svg"
-                alt=""
-                width={20}
-                height={20}
-                className="mb-2"
-              />
-              <h3>Community Focused</h3>
-              <p>Quality over quantity.</p>
+            <div className="flex flex-col items-center gap-4 w-full h-full border border-gray-100/10 p-4 rounded-2xl bg-gray-100/5 shadow-sm">
+              <Image src="/users-round.svg" alt="" width={20} height={20} />
+              <div className="text-center">
+                <h3>Community Focused</h3>
+                <p className="font-extralight">Quality over quantity.</p>
+              </div>
             </div>
           </div>
         </div>
