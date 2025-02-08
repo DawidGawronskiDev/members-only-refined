ad052584847d26cc0a3a71c9df1ed766a890464f
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-02-08 13:42:42 +0000

    update background styling

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,8 +1,8 @@
 export default function Background() {
   return (
-    <div className="absolute h-full z-[-1] w-dvw overflow-hidden">
+    <div className="absolute h-full z-[-1] w-full overflow-hidden">
       <div className="w-[940px] aspect-square rounded-full bg-accentSecondary absolute left-1/2 -translate-x-[calc(50%+400px)] -top-[500px] blur-[200px] transform-gpu" />
-      <div className="w-[1300px] aspect-square rounded-full bg-accentPrimary absolute left-1/2 -translate-x-[calc(50%-400px)] -top-[400px] blur-[200px] transform-gpu" />
+      <div className="w-[1300px] aspect-square rounded-full bg-accentPrimary absolute left-1/2 -translate-x-[calc(50%-450px)] -top-[400px] blur-[200px] transform-gpu" />
     </div>
   );
 }
