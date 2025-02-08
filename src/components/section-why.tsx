b72f7e6d0affac6072e7032336d86fa8bf45754a
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-02-08 16:54:41 +0000

    create card

--- src/components/section-why.tsx
+++ src/components/section-why.tsx
@@ -8,32 +8,44 @@ export default function SectionWhy() {
         <div className="flex flex-col items-center gap-16">
           <h2>Why Join?</h2>
           <div className="w-full grid md:grid-cols-3 md:flex-row items-center md:items-start gap-4">
-            <div className="flex flex-col items-center gap-4 w-full h-full border border-gray-100/10 p-4 rounded-2xl bg-gray-100/5 shadow-sm">
-              <Image src="/sparkle.svg" alt="" width={20} height={20} />
-              <div className="text-center">
-                <h3>Exclusive Access</h3>
-                <p className="font-extralight">Not everyone gets in.</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center gap-4 w-full h-full border border-gray-100/10 p-4 rounded-2xl bg-gray-100/5 shadow-sm">
-              <Image src="/shield.svg" alt="" width={20} height={20} />
-              <div className="text-center">
-                <h3>Privacy First</h3>
-                <p className="font-extralight">
-                  No public visibility, no distractions.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center gap-4 w-full h-full border border-gray-100/10 p-4 rounded-2xl bg-gray-100/5 shadow-sm">
-              <Image src="/users-round.svg" alt="" width={20} height={20} />
-              <div className="text-center">
-                <h3>Community Focused</h3>
-                <p className="font-extralight">Quality over quantity.</p>
-              </div>
-            </div>
+            <Card
+              title="Exclusive Access"
+              description="Not everyone gets in."
+              image="/sparkle.svg"
+            />
+            <Card
+              title="Privacy First"
+              description="No public visibility, no distractions."
+              image="/shield.svg"
+            />
+            <Card
+              title="Community Focused"
+              description="Quality over quantity."
+              image="/users-round.svg"
+            />
           </div>
         </div>
       </MaxWidth>
     </section>
+  );
+}
+
+export function Card({
+  title,
+  description,
+  image,
+}: {
+  title: string;
+  description: string;
+  image: string;
+}) {
+  return (
+    <div className="flex flex-col items-center gap-4 w-full h-full border border-gray-100/10 p-4 rounded-2xl bg-gray-100/5 shadow-sm">
+      <Image src={image} alt="" width={20} height={20} />
+      <div className="text-center">
+        <h3>{title}</h3>
+        <p className="font-extralight">{description}</p>
+      </div>
+    </div>
   );
 }
