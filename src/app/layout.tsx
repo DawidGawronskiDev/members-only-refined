8d6b4316360264889621e582aa31f9b9afe4b69a
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-02-09 16:31:35 +0000

    add suppressHydrationWarning to html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} antialiased bg-[center_-1000px] bg-no-repeat`}
         style={{ backgroundImage: "url('/background-lights.png')" }}
