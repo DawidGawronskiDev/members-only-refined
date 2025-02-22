fab72db69ee790762467a8468c6d729c2cdae104
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-02-22 12:17:40 +0000

    create signup page

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import LogoutButton from "@/components/logout-button";
+import GoogleButton from "@/components/google-button";
+
+export default function SignUp() {
+  return (
+    <div>
+      <GoogleButton />
+      <LogoutButton />
+    </div>
+  );
+}
