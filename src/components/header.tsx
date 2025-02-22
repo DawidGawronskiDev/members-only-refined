b8d3c43abd5869aabe5bedc77d87e0e6836e6b5f
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-02-22 12:50:14 +0000

    add messages link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import UserMenu from "./user-menu";
 
 export default function Header() {
   return (
     <header className="sticky top-0 left-0 px-4 py-2">
+      <Link href="/messages">Messages</Link>
       <UserMenu />
     </header>
   );
