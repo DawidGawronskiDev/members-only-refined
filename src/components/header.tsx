cf51b761d10f9ffed8514e5f65eec8d8a010ad71
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-02-22 12:23:32 +0000

    create header

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import UserMenu from "./user-menu";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 left-0 px-4 py-2">
+      <UserMenu />
+    </header>
+  );
+}
