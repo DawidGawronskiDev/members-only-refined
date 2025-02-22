5652577819367efdd6d82cbdd813b30479ef2980
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-02-22 12:38:08 +0000

    create handleUserChange

--- src/store/auth-provider.tsx
+++ src/store/auth-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { removeCookies, setCookies } from "@/actions/cookies";
 import { auth } from "@/lib/firebase-client";
 import { onAuthStateChanged, type User } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
@@ -10,13 +11,25 @@ export interface AuthContextProps {
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
+const handleUserChange = async (user: User | null) => {
+  if (user) {
+    const tokenResult = await user.getIdTokenResult();
+    const token = tokenResult.token;
+    const refreshToken = user.refreshToken;
+    await setCookies({ token, refreshToken });
+  } else {
+    await removeCookies();
+  }
+};
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   console.log(currentUser);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      await handleUserChange(user);
       setCurrentUser(user);
     });
     return () => unsubscribe();
