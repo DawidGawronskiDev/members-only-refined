a023ed2f921501a4db10a98d524990025ecb1038
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-04-17 13:47:03 +0100

    delete files

--- src/components/logout-button.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { signOut } from "firebase/auth";
-import { Button } from "@/components/ui/button";
-import { auth } from "@/lib/firebase-client";
-
-export default function LogoutButton() {
-  return <Button onClick={async () => await signOut(auth)}>Log out</Button>;
-}
