ba4d8aa4507a5c379af90c0985d385608d2b3168
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-04-18 17:21:52 +0100

    create get posts api route

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,59 @@
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { User } from "@/app/generated/prisma";
+
+export async function GET() {
+  /**
+   * Valide session
+   */
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({
+      status: 404,
+      error: { message: "Unauthorized" },
+    });
+  }
+
+  const u = session.user as unknown as User;
+
+  /**
+   * Valide user
+   */
+  const existingUser = await prisma.user.findUnique({
+    where: { id: Number(u.id) },
+  });
+
+  if (!existingUser) {
+    return NextResponse.json({
+      status: 404,
+      error: { message: "Unauthorized" },
+    });
+  }
+
+  /**
+   * Validate membership
+   */
+  if (!existingUser.isMember) {
+    return NextResponse.json({
+      status: 404,
+      error: { message: "Unauthorized" },
+    });
+  }
+
+  /**
+   * Get posts
+   */
+  const posts = await prisma.post.findMany();
+
+  if (!posts) {
+    return NextResponse.json({
+      status: 500,
+      error: { message: "Internal server error" },
+    });
+  }
+
+  return NextResponse.json({ status: 200, data: posts });
+}
