f122be847823c52e408b077c7cd90ccf660401ea
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-04-22 16:37:18 +0100

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,8 +5,27 @@ import MaxWidthContanier from "./max-width-container";
 import { ThemeToggle } from "./theme-toggle";
 import Navigation from "./navigation";
 import Auth from "./auth";
+import { useEffect, useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
 
+import { Settings } from "lucide-react";
 export default function Header() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 512);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <header className="sticky top-0 z-50 w-full border border-background/10 bg-background backdrop-blur supports-[backdrop-filter]:bg-background/10">
       <MaxWidthContanier>
@@ -14,15 +33,33 @@ export default function Header() {
           <div className="flex-1/3">
             <Logo />
           </div>
-          <div className="flex items-center justify-between flex-2/3 *:flex-1">
-            <div className="text-center">
-              <Navigation />
+          {isMobile && (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Settings />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="max-w-min">
+                <div className="flex flex-col items-end gap-4">
+                  <Navigation />
+                  <Auth />
+                  <ThemeToggle />
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
+          {!isMobile && (
+            <div className="flex items-center justify-between flex-2/3 *:flex-1">
+              <div className="text-center">
+                <Navigation />
+              </div>
+              <div className="flex items-center justify-end gap-4">
+                <ThemeToggle />
+                <Auth />
+              </div>
             </div>
-            <div className="flex items-center justify-end gap-4">
-              <ThemeToggle />
-              <Auth />
-            </div>
-          </div>
+          )}
         </div>
       </MaxWidthContanier>
     </header>
