194ad453d1b670a7c37afc3c62eaa3f37d48edd4
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2025-04-22 15:47:17 +0100

    update navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,6 +3,8 @@
 import { User } from "@/app/generated/prisma";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { toast } from "sonner";
+import Membership from "./membership";
 
 export default function Navigation() {
   const session = useSession();
@@ -10,16 +12,25 @@ export default function Navigation() {
 
   return (
     <nav>
-      <ul>
-        <li>
-          <Link
-            href={"/posts"}
-            onClick={(e) => (user?.isMember ? null : e.preventDefault())}
-          >
-            Posts
-          </Link>
-        </li>
-      </ul>
+      {!user?.isMember ? (
+        <Membership />
+      ) : (
+        <ul>
+          <li>
+            <Link
+              href={"/posts"}
+              onClick={(e) => {
+                if (!user?.isMember) {
+                  toast.error("You need to be a member to access this page.");
+                  e.preventDefault();
+                }
+              }}
+            >
+              Posts
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
